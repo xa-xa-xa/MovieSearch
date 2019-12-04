7f94cc0bfee7249657c646699183db40a5c86e81
xa-xa-xa <askrasn@gmail.com>
2019-12-04 08:39:08 -0800

    added animation to cast

--- src/components/layout/Cast/Cast.js
+++ src/components/layout/Cast/Cast.js
@@ -1,51 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
-import styles from './cast.module.scss';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import styles from './cast.module.scss';
 
 const no_image = '/images/no_image-4.png';
 
 const Cast = ({ cast }) => {
   const [isExpanded, setIsExpanded] = useState(false);
 
-  const castNumberToShow = isExpanded ? cast.length : 10;
+  const castNumberToShow = isExpanded ? 21 : 6;
 
   if (cast === undefined || !cast.length) {
     return <span>Sorry, no info available yet... </span>;
   } else {
-    return (
-      <ul className={styles.cast}>
-        {cast
-          .filter(i => i.order < castNumberToShow)
-          .map(i => (
-            <li className={styles.profile} key={i.id}>
-              <Link to={`/overview/person/${i.id}`} className='details'>
-                <img
-                  className={styles.portrait}
-                  src={
-                    i.profile_path
-                      ? `https://image.tmdb.org/t/p/w300/${i.profile_path}`
-                      : no_image
-                  }
-                  alt={i.name}
-                />
+    const items = cast
+      .filter(i => i.order < castNumberToShow)
+      .map((item, idx) => (
+        <CSSTransition key={idx} classNames='item' timeout={100}>
+          <div className={styles.profile} key={idx}>
+            <Link to={`/overview/person/${item.id}`} className='details'>
+              <img
+                className={styles.portrait}
+                src={
+                  item.profile_path
+                    ? `https://image.tmdb.org/t/p/w300/${item.profile_path}`
+                    : no_image
+                }
+                alt={item.name}
+              />
+              {item.name}
+            </Link>
+          </div>
+        </CSSTransition>
+      ));
 
-                {i.name}
-              </Link>
-            </li>
-          ))}
+    // View
+    return (
+      <Fragment>
+        <TransitionGroup className={styles.cast}>{items}</TransitionGroup>
+
         <div className={styles.cast_expand}>
           <FontAwesomeIcon
             onClick={() => setIsExpanded(!isExpanded)}
             className={styles.chevron_icon}
-            icon={isExpanded ? faChevronUp : faChevronDown}></FontAwesomeIcon>
+            icon={isExpanded ? faChevronUp : faChevronDown}
+          />
         </div>
-      </ul>
+      </Fragment>
     );
   }
 };
-
 export default Cast;
